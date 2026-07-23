16054e95bbc481da5554d51ab590789b4c4cc461
ray-adas2 <1533735779@qq.com>
2026-07-23 16:16:53 +0800

    以服务器为准同步

--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -2,7 +2,7 @@ import type { PioConfig } from "../types/config";
 
 // Pio 看板娘配置
 export const pioConfig: PioConfig = {
-	enable: false, // 启用看板娘
+	enable: true, // 启用看板娘
 	models: ["/pio/models/NOIR/noir.model3.json"], // 默认模型路径
 	position: "left", // 模型位置
 	width: 280, // 默认宽度
@@ -11,16 +11,16 @@ export const pioConfig: PioConfig = {
 	hiddenOnMobile: true, // 默认在移动设备上隐藏
 	hideAboutMenu: false, // 隐藏内置 About 菜单按钮
 	dialog: {
-		welcome: "Welcome to Mizuki Website!", // 欢迎词
+		welcome: "欢迎来到我的博客！", // 欢迎词
 		touch: [
-			"What are you doing?",
-			"Stop touching me!",
-			"HENTAI!",
-			"Don't bully me like that!",
+			"你在干嘛呢？",
+			"别碰我啦！",
+			"变态！",
+			"不要欺负我!",
 		], // 触摸提示
-		home: "Click here to go back to homepage!", // 首页提示
-		skin: ["Want to see my new outfit?", "The new outfit looks great~"], // 换装提示
-		close: "QWQ See you next time~", // 关闭提示
+		home: "点这里回到首页哦~", // 首页提示
+		skin: ["想看我的新衣服吗？", "新衣服好看吗~"], // 换装提示
+		close: "QWQ 下次见~", // 关闭提示
 		link: "https://github.com/LyraVoid/Mizuki", // 关于链接
 	},
 };
